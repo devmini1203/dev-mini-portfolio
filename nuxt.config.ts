777f2c7725a28afa56d906af77a9cf65a7292c39
devmini1203 <devmini1203@gmail.com>
2023-11-18 12:40:59 +0900

    Chore: @pinia/nuxt 삭제 및 index.vue 추가 pinia/nuxt 모듈 삭제 루트페이지 추가 app.vue nuxtLayout 및 nuxtPage 반영 default layout 생성

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@element-plus/nuxt', '@pinia/nuxt'],
-  ssr: false
+  modules: ['@element-plus/nuxt'],
+  ssr: false,
+  app: {
+    baseURL: '/dev_mini_portfolio',
+    head: {
+      title: 'JM Portfolio',
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, user-scalable=no'
+        },
+        {
+          name: 'google',
+          content: 'notranslate'
+        },
+        {
+          charset: 'utf-8'
+        }
+      ]
+    }
+  }
 });
