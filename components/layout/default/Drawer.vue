c27a77d4be03fdd218403069d6c2e54dfc16fa10
devmini1203 <devmini1203@gmail.com>
2023-11-19 18:42:35 +0900

    Chore: vueuse 모듈 설치 및 default 레이아웃 수정 vueuse 모듈 설치 default 레이아웃에 header,footer,drawer 컴포넌트 추가

---
+++ components/layout/default/Drawer.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: /layout/default/Drawer
+  </div>
+</template>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,8 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Header from '@/components/layout/default/Header.vue';
+import Footer from '@/components/layout/default/Footer.vue';
+import Drawer from '@/components/layout/default/Drawer.vue';
+</script>
 
 <template>
   <div>
