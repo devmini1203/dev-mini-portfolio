ec79fba88aeee3dc84b28a858620dd7e6f7bea74
devmini1203 <devmini1203@gmail.com>
2023-12-06 20:31:53 +0900

    Desgin: about-me 컨텐츠 이동 버튼 생성 및 css 적용 - 1. About me 컨텐츠로 이동 버튼 구현 - 2. (1) 을 위한 함수 구현 - 3. chrvrons button css 및 animation 구현

--- components/content/Banner.vue
+++ components/content/Banner.vue
@@ -1,15 +1,24 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { IconChevronsDown } from '@tabler/icons-vue';
+
+/**
+ * About Me 컨텐츠로 이동
+ */
+const moveToAboutMeContent = () => {
+  document.querySelector('.about-me-container')?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+};
+</script>
 
 <template>
   <div class="banner-container">
     <div class="banner-title-box">
-      <div class="top-text-box">
-        <h1>Welocme!</h1>
-      </div>
+      <div class="top-text-box">Welocme!</div>
 
-      <div class="bottom-text-box">
-        <h1>JONGMIN'S PORTFOLIO</h1>
-      </div>
+      <div class="bottom-text-box">JONGMIN'S PORTFOLIO</div>
+    </div>
+
+    <div class="next-content-button-box">
+      <component :is="IconChevronsDown" class="chevrons-down-button" @click="moveToAboutMeContent"> </component>
     </div>
   </div>
 </template>
