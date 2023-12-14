378758372fef8a903e8bcfa7d08253d4761a8d57
devmini1203 <devmini1203@gmail.com>
2023-12-14 22:43:38 +0900

    Add: Autonics CI img 추가

--- components/content/Career.vue
+++ components/content/Career.vue
@@ -1,7 +1,32 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="career-container">Component: /main/Career</div>
+  <div class="career-container">
+    <div class="title-box">
+      <span class="title"> CAREER </span>
+    </div>
+
+    <div class="my-career-box">
+      <div>
+        <NuxtImg src="/img/career/AutonicsCI.png" />
+      </div>
+
+      <div style="color: white; padding-left: 3rem">
+        <div style="font-size: 1.5rem; padding-bottom: 0.5rem">오토닉스</div>
+        <div style="padding-bottom: 1rem">2021.01.01 ~ ing</div>
+
+        <p>오토닉스는 산업 자동화 분야의 핵심인 센서, 제어기기, 모션 디바이스, 레이저 마킹 시스템 등</p>
+        <p>약 6,000여 종의 제품을 생산, 판매하며 종합적인 솔루션을 제공하고 있는 산업 자동화 전문 기업입니다.</p>
+        <br />
+
+        <p>개발된지 오래된 백오피스 사이트 및 대리점 주문 사이트를 포함한 B2B 사이트 그리고 공장 라인에서 사용되는 Gathering 프로그램등을</p>
+        <p style="word-break: keep-all">
+          VUE,NUXT 프레임워크를 사용해 마이그레이션하는 프로젝트에서 프론트엔드와 백엔드 개발을 담당하고 있으며, 리눅스 및 윈도우 서버의 관리 및 배포 작업 또한
+          맡고 있습니다.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- components/content/Projects.vue
+++ components/content/Projects.vue
@@ -1,7 +1,11 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="projects-container">Component: /main/Projects</div>
+  <div class="projects-container">
+    <div class="title-box">
+      <span class="title"> PROJECTS </span>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
