c05ce573b418f681dfaa3682d63e4dcb118f97ce
devmini1203 <devmini1203@gmail.com>
2023-12-26 23:28:30 +0900

    Edit: projects 컨텐츠 수정 등 - 1. projects 컨텐츠 중 [국내 공장 Gathering 사이트] 구현 완료 - 2. (1)을 위한 프로젝트 이미지 파일 추가 - 3. projects container css 일부 수정 - 4. projects 컨텐츠 중 [포트폴리오 사이트]에 사용될 이미지 파일 추가 및 적용

--- components/content/Projects.vue
+++ components/content/Projects.vue
@@ -37,24 +37,42 @@ onMounted(() => {
           <Swiper
             :loop="true"
             :navigation="true"
+            :effect="'fade'"
             :modules="swiperModule"
             :autoplay="{
-              delay: 3000,
+              delay: 4000,
               disableOnInteraction: false
+            }"
+            :style="{
+              '--swiper-navigation-color': '#000000',
+              '--swiper-pagination-color': '#000000',
+              '--swiper-navigation-size': '24px'
             }"
             :pagination="{
               clickable: true
             }"
           >
-            <SwiperSlide><p>1</p></SwiperSlide>
-            <SwiperSlide><p>2</p></SwiperSlide>
-            <SwiperSlide><p>3</p></SwiperSlide>
-            <SwiperSlide><p>4</p></SwiperSlide>
-            <SwiperSlide><p>5</p></SwiperSlide>
-            <SwiperSlide><p>6</p></SwiperSlide>
+            <SwiperSlide>
+              <NuxtImg src="/img/projects/gathering/web_gathering_01.png" class="swiper-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <NuxtImg src="/img/projects/gathering/web_gathering_02.png" class="swiper-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <NuxtImg src="/img/projects/gathering/web_gathering_03.png" class="swiper-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <NuxtImg src="/img/projects/gathering/web_gathering_04.png" class="swiper-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <NuxtImg src="/img/projects/gathering/web_gathering_05.png" class="swiper-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <NuxtImg src="/img/projects/gathering/web_gathering_06.png" class="swiper-img" />
+            </SwiperSlide>
           </Swiper>
 
-          <div class="important-message-box">※ 해당 프로젝트 이미지의 모든 데이터는 임의로 생성한 목업 데이터 입니다. ※</div>
+          <div class="important-message-box">※ 해당 프로젝트 이미지에 나타나는 모든 데이터는 임의로 생성한 목업 데이터 입니다. ※</div>
         </div>
 
         <div class="projects-desc-box">
@@ -66,7 +84,7 @@ onMounted(() => {
           <div class="project-explan-box">
             <p>
               오토닉스는 생산직 근로자분들에게 C#으로 개발된 실적 등록 및 공정 관리 프로그램을 윈도우 테블릿에서 제공하고 있었습니다. 그러나 계속되는 윈도우
-              테블릿의 가격 상승 및 향후 공급 불안정이 예상되어, 비교적 저렴한 안드로이드 테블릿으로의 전환이 필요하게 되었고
+              테블릿의 가격 상승 및 향후 공급 불안정이 예상되어, 비교적 저렴한 안드로이드 테블릿으로의 전환이 필요하게 되었습니다.
             </p>
             <p>
               이에 따라, <span class="skill-emphasize">VUE2</span> 프레임워크와 UI프레임워크인 <span class="skill-emphasize">BootstrapVue</span>를 사용하여 C#
@@ -82,9 +100,9 @@ onMounted(() => {
             <p>또한 현장 관리자들과의 소통을 통해, 다양한 요구 사항 및 오류에 대응하며 CS와 QA 업무의 중요성도 느끼게 되었습니다.</p>
 
             <p class="closing-ment">
-              첫 실무 프로젝트이자 혼자서 프론트엔드를 담당했다보니 실수도 많았고 돌아보면 아쉬움도 많은 프로젝트입니다. 하지만 웹 개발 직군을 선택한 것이 옳은
-              결정이었음을 다시 한번 느끼게 해줄 정도의 즐거움과 만족감을 느끼게 해준 프로젝트였으며, 자바스크립트 기본기를 탄탄히 다지게 해주었고 VUE2
-              프레임워크의 숙련도를 높여준 고마운 프로젝트임에는 틀림없습니다.
+              첫 실무 프로젝트이자 혼자서 프론트엔드를 담당했다보니 실수도 많았고 돌아보면 우아하지 못한 UI/UX 등 아쉬움이 많은 프로젝트입니다. 하지만 웹 개발
+              직군을 선택한 것이 옳은 결정이었음을 다시 한번 느끼게 해줄 정도의 즐거움과 만족감을 느끼게 해준 프로젝트였으며, 자바스크립트 기본기를 탄탄히
+              다지게 해주었고 VUE2 프레임워크의 숙련도를 높여준 고마운 프로젝트임에는 틀림없습니다.
             </p>
           </div>
 
@@ -99,7 +117,9 @@ onMounted(() => {
             </div>
             <div class="detail">
               <div class="label-box">주요 기능</div>
-              <div class="value-box"></div>
+              <div class="value-box">
+                실적 등록, 작업 지시 검색, 작업자 전환 기능, 자재 등록 및 관리, 실적 확인, 불량/폐기 등록 및 관리, 가상 키패드, 작업자 추가, 작업지도서 확인
+              </div>
             </div>
             <div class="detail">
               <div class="label-box">Front-End</div>
@@ -470,6 +490,9 @@ onMounted(() => {
             </SwiperSlide>
             <SwiperSlide>
               <NuxtImg src="/img/projects/portfolio/skills.png" class="swiper-img" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <NuxtImg src="/img/projects/portfolio/projects.png" class="swiper-img" />
             </SwiperSlide>
           </Swiper>
         </div>
