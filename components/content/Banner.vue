23628619553a3cb53674cb3811344a7848f7e458
devmini1203 <devmini1203@gmail.com>
2023-12-26 23:34:23 +0900

    Edit: banner 다이나믹 클래스 해제 등 - 1. 다이나믹 클래스 바인딩으로 적용하던 css를 media query로 대체 - 2. (1)을 적용한 후 필요 없어진 css 클래스 및 script setup의 로직들 삭제

--- components/content/Banner.vue
+++ components/content/Banner.vue
@@ -1,30 +1,16 @@
 <script lang="ts" setup>
 import { IconChevronsDown } from '@tabler/icons-vue';
 
-const props = defineProps<{
-  appWidth: number;
-}>();
-
 const elementsStore = useElementsStore();
 const bannerRef = ref<HTMLElement | null>(null);
-
-const dynamicBannerClass = computed(() => {
-  if (props.appWidth >= 730) {
-    return 'banner-container';
-  } else {
-    return 'banner-container-minimize';
-  }
-});
 
 onMounted(() => {
   elementsStore.bannerRef = bannerRef.value;
 });
-
-//.banner-title-box-minimize
 </script>
 
 <template>
-  <div :class="[dynamicBannerClass, 'common-gradient-background']" ref="bannerRef">
+  <div class="banner-container common-gradient-background" ref="bannerRef">
     <div class="banner-title-box">
       <div class="top-text-box">WELCOME!</div>
 
