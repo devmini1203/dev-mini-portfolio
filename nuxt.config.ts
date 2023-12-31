a54a11e6f920ec9e246327a0622bda4c88d1d7d7
devmini1203 <devmini1203@gmail.com>
2023-12-31 19:07:24 +0900

    Chore: config.js 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     baseURL: '/dev_mini_portfolio/',
+    buildAssetsDir: 'dev_mini_portfolio',
     head: {
       title: 'JM Portfolio',
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/dev_mini_portfolio/favicon.ico' }],
